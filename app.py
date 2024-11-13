860da9eedd01c29feca6f4ad90ffc991249b6b39
JoseDanielRestrepoOrozco <jose.resoro4789@gmail.com>
2024-11-13 17:24:39 -0500

    agragando columnas

--- app.py
+++ app.py
@@ -7,30 +7,36 @@ st.title("Asistente virtual")
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
-# Mostrar mensajes de chat desde el historial
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
+col1, col2, col3 = st.columns([1,2,1])
 
-audio_value = st.audio_input("Comienza a hablar")
+with col1:
+    audio_value = st.audio_input("Comienza a hablar")
+with col2:
+    # Mostrar mensajes de chat desde el historial
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
 
-# Procesar entrada de audio si se ha grabado algo
-if audio_value:
-    prompt = groqService.transcribir_audio(audio_value)
+    # Procesar entrada de audio si se ha grabado algo
+    if audio_value:
+        prompt = groqService.transcribir_audio(audio_value)
 
-    # Agregar mensaje del usuario al historial de chat
-    st.session_state.messages.append({"role": "user", "content": prompt})
+        # Agregar mensaje del usuario al historial de chat
+        st.session_state.messages.append({"role": "user", "content": prompt})
 
-    # Mostrar mensaje del usuario en el contenedor de mensajes
-    with st.chat_message("user"):
-        st.markdown(prompt)
+        # Mostrar mensaje del usuario en el contenedor de mensajes
+        with st.chat_message("user"):
+            st.markdown(prompt)
 
-    # Generar una respuesta (aquí solo se devuelve un eco de la entrada del usuario)
-    response = f"Echo: {prompt}"
+        # Generar una respuesta (aquí solo se devuelve un eco de la entrada del usuario)
+        response = f"Echo: {prompt}"
 
-    # Mostrar respuesta del asistente en el contenedor de mensajes
-    with st.chat_message("assistant"):
-        st.markdown(response)
+        # Mostrar respuesta del asistente en el contenedor de mensajes
+        with st.chat_message("assistant"):
+            st.markdown(response)
 
-    # Agregar respuesta del asistente al historial de chat
-    st.session_state.messages.append({"role": "assistant", "content": response})
+        # Agregar respuesta del asistente al historial de chat
+        st.session_state.messages.append({"role": "assistant", "content": response})
+
+with col3:
+    st.markdown('espacio para video')
